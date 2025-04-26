67e1d46db1a2d2cbb02fc26fba0c4df50b97735f
Ezekielvita23 <e.vita26@hotmail.com>
2025-04-26 14:48:33 +0200

    Add files via upload

---
+++ Milkshakee.py
@@ -0,0 +1,13 @@
+# Do not modify these lines
+__winc_id__ = 'e75b6cd4a7404e3ca76c308566dafb5d'
+__human_name__ = 'recipe'
+
+# Add your code after this line
+print(' Strawberry milkshake recipe')
+print('-------------------------------') 
+print('100g vanilla ice cream about three scoops')
+print('200g frozen strawberries') 
+print('300ml milk') 
+print('2 teaspoons caster sugar') 
+print('whipped cream') 
+print('---------------------------------')
